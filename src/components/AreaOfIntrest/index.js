824a5a0a190e8e2a0a11ab24cbabb4181b659663
Praveen Kumar Chundi <praveen.chundi@infodatinc.com>
2017-12-19 16:51:24 -0600

    area of intrest account to the mock up

--- src/components/AreaOfIntrest/index.js
+++ src/components/AreaOfIntrest/index.js
@@ -2,15 +2,21 @@
 import React from 'react';
 import ss from './styles.scss';
 import PureRenderMixin  from 'react-addons-pure-render-mixin';
-
+let cateGoryObj = {
+    'Supply Chain':['Supply 1','Supply 2','Supply 3','Supply 4','Supply 5','Supply 6','Supply 7','Supply 8'],
+    'Procurement':['Procurement 1','Procurement 2','Procurement 3','Procurement 4','Procurement 5','Procurement 6','Procurement 7','Procurement 8'],
+    'Manufacturing':['Manufacturing 1','Manufacturing 2','Manufacturing 3','Manufacturing 4','Manufacturing 5','Manufacturing 6','Manufacturing 7','Manufacturing 8'],
+    'Finance':['Finance 1','Finance 2','Finance 3','Finance 4','Finance 5','Finance 6','Finance 7','Finance 8'],
+    'Human Resources':['Human 1','Human 2','Human 3','Human 4','Human 5','Human 6','Human 7','Human 8'],
+    'Marketing':['Marketing 1','Marketing 2','Marketing 3','Marketing 4','Marketing 5','Marketing 6','Marketing 7','Marketing 8']
+};
 export default React.createClass({
 mixins: [PureRenderMixin],
 getInitialState() {
     return {
         mouserHover:false,
-        revenue:'',
-        industry:'',
-        employeeSize:''
+        subCateGory:cateGoryObj['Supply Chain'],
+        category:cateGoryObj
     }
 },
 componentWillMount() {
@@ -22,28 +28,46 @@ componentWillUnmount() {
 goToPersonalInfoPage(){
     this.props.goToPersonalInfoPage();
 },
-createSubCategory(cat){
-    let subCategory = 8;
+createSubCategory(){
+    let subCategory = this.state.subCateGory;
     let arr = [];
-    for(let i = 0; i< subCategory ; i++){
-        arr.push(<div key={cat + i}><div className="subCategory">
-        {"Subcategory - " + i}
+    for(let i in subCategory){
+        arr.push(<div key={subCategory[i]}><div className="subCategory">
+        {subCategory[i]}
         </div></div>);
     }
     return arr;
 },
-createCategory(){
+// createCategory(){
+//     let category = ["Supply Chain","Procurement","Manufacturing","Finance","Human Resources","Marketing"];
+//     let arr = [];
+//     for(let i in category){
+//         arr.push(
+//             <div key = {category[i]} className="cateGory">
+//                 <h3>{category[i]}</h3>
+//                 <div className="flexFlow">
+//                     <div className="subWrapper">
+//                         {this.createSubCategory(category[i])}
+//                     </div>
+//                 </div>
+//             </div>
+//         );
+//     }
+//     return arr;
+// },
+categorySelection(e){
+    let key = e.target.id;
+    let category = this.state.category;
+    let newSubCategory = category[key];
+    this.setState({subCateGory:newSubCategory});
+},
+createCategoryBtns(){
     let category = ["Supply Chain","Procurement","Manufacturing","Finance","Human Resources","Marketing"];
     let arr = [];
     for(let i in category){
         arr.push(
-            <div key = {category[i]} className="cateGory">
-                <h3>{category[i]}</h3>
-                <div className="flexFlow">
-                    <div className="subWrapper">
-                        {this.createSubCategory(category[i])}
-                    </div>
-                </div>
+            <div key = {category[i]} id={category[i]} onClick={this.categorySelection} className="cateGory">
+                <h3 key = {category[i]} id={category[i]}>{category[i]}</h3>
             </div>
         );
     }
@@ -65,9 +89,18 @@ render() {
                 <section className="MainContent">
                     <p>Select Areas of Interests From the Categories below.</p>
                 </section>
+                <center className="categoryBtn">
+                    {this.createCategoryBtns()}
+                </center>
                 <div className="AllCateGory">
                     <div className="wrapper">
-                        {this.createCategory()}
+                        <div className="subCateGory">
+                            <div className="flexFlow">
+                                <div className="subWrapper">
+                                    {this.createSubCategory()}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <center>
@@ -89,3 +122,9 @@ render() {
     );
 },
 });
+
+{/* <div className="AllCateGory">
+<div className="wrapper">
+    {this.createCategory()}
+</div>
+</div> */}
