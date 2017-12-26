ebd64ff89a6021664f7e6ed6e759324544270d62
praveen <praveen.chundi13@gmail.com>
2017-12-25 23:10:46 -0600

    personal infomation

--- src/components/PersonalInfo/index.js
+++ src/components/PersonalInfo/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import ss from './styles.scss';
 import PureRenderMixin  from 'react-addons-pure-render-mixin';
+import { bindListener } from 'utils';
+
 
 export default React.createClass({
 mixins: [PureRenderMixin],
@@ -16,6 +18,14 @@ componentWillMount() {
 componentWillUnmount() {
     ss.unuse();
 },
+componentDidMount() {
+
+},
+editProfile(e){
+   let id = e.target.id;
+   this.props.editProfile(id);
+},
+
 goToPersonalInfoPage(){
     this.props.goToPersonalInfoPage();
 },
@@ -24,38 +34,90 @@ render() {
     let submitEnabled = ( this.state.passwordSuccess && this.state.confirmPasswordSuccess && this.state.secQuestion !== '' && this.state.secAnswerSuccess ) ? "submitBtn EnableBtn" : "submitBtn DisableBtn";
     let nextClass = this.state.mouserHover ? "getStarted next DisableBtn" : "getStarted next";
     return (
+	<div className="addProfilebg">
         <div className="QRZT_BasicModal" onClick={this.props.close}>
             <div className="ModalWrap">
                 <section className="MainContent">
-                    <p>2 of 5</p>
+                    <p>2 of 4</p>
                 </section>
                 <center>
                     <p className="heading">Personal Information</p>
                 </center>
-                <section className="MainContent">
-                    <div className="nonEditable">NAME</div>
-                    <div className="nonEditable value">{this.props.userInfo.userName}</div>
-                    <div className="nonEditable">COMPANY</div>
-                    <div className="nonEditable value">{this.props.userInfo.company}</div>
-                    <div className="nonEditable">PRIMARY JOB FUNCTION</div>
-                    <div className="nonEditable value">{this.props.userInfo.jobFunction}</div>
+                <section className="MainContent space-txt">
+					<div className="col-lg-12">
+						<div className="nonEditable">NAME</div>
+						<div className="nonEditable value">{this.props.userInfo.userName}</div>
+						<div className="nonEditable">COMPANY</div>
+						<div className="nonEditable value">{this.props.userInfo.company}</div>
+						<div className="nonEditable">PRIMARY JOB FUNCTION</div>
+						<div className="nonEditable value">{this.props.userInfo.jobFunction}</div>
+					</div>
+					<div className="clearfix"></div>
+					<div className="col-lg-12">					
+						<input className={"noErrorField"} id="invitationNumber" type="text"/>
+						<field-label>TITLE</field-label>
+					</div>
+					<div className="clearfix"></div>
+					<div className="col-lg-12">	<span>From</span></div>
+				    <div className="clearboth"></div>
+					<div className="field-txt col-lg-6">
+						<input className={"noErrorField"} id="jFunction" type="text" onChange={()=>{return}}/>
+                        <span className="fa fa-caret-down downArrow drp-btn"></span>
+                        <field-label>MONTH</field-label>
+					</div>
+					<div className="field-txt col-lg-6">
+						<input className={"noErrorField"} id="jFunction" type="text" onChange={()=>{return}}/>
+                        <span className="fa fa-caret-down downArrow drp-btn"></span>
+                        <field-label>YEAR</field-label>
+					</div>
+					<div className="clearfix"></div>
+					<div className="col-lg-12">	<span>To</span></div>
+				    <div className="clearboth"></div>
+					<div className="field-txt col-lg-6">
+						<input className={"noErrorField"} id="jFunction" type="text" onChange={()=>{return}}/>
+                        <span className="fa fa-caret-down downArrow drp-btn"></span>
+                        <field-label>MONTH</field-label>
+					</div>
+					<div className="field-txt col-lg-6">
+						<input className={"noErrorField"} id="jFunction" type="text" onChange={()=>{return}}/>
+                        <span className="fa fa-caret-down downArrow drp-btn"></span>
+                        <field-label>YEAR</field-label>
+					</div>
+					<div className="clearboth"></div>					
+					<div className="col-lg-12">
+					<div className="space-txt">
+						<textarea className={"noErrorField txt-area"} id="invitationNumber" type="text"/>
+							<field-label>BIO</field-label>
+						</div>
+					</div>
+					<div className="col-lg-12">
+						<label id="addWork" onClick={this.editProfile} className="userName experiance addMore">
+							<i id="addWork" onClick={this.editProfile} className="fa fa-plus" aria-hidden="true"></i> Previous Work Experience
+						</label>
+					</div>
+					<div className="col-lg-6 field-txt">
+						<button className="previous" 
+						onMouseOver={()=>{
+							this.setState({mouserHover:true});
+						}}
+						onMouseOut={()=>{
+							this.setState({mouserHover:false});
+						}}
+						onClick={this.props.goBack}>PREVIOUS</button>
+					</div>
+					<div className="col-lg-6 field-txt">
+						<button className="next" onClick={this.props.goToCompanyInfoPage}>NEXT</button>
+					</div>
+					<div className="clearboth"></div>					
+					<div className="col-lg-12">
+						<center>
+							<label className="skip" onClick={this.props.goToCompanyInfoPage} >Skip this step</label>
+						</center>
+					</div>
                 </section>
-                <center>
-                    <button className="getStarted previous" 
-                    onMouseOver={()=>{
-                        this.setState({mouserHover:true});
-                    }}
-                    onMouseOut={()=>{
-                        this.setState({mouserHover:false});
-                    }}
-                    onClick={this.props.goBack}>PREVIOUS</button>
-                    <button className={nextClass} onClick={this.props.goToCompanyInfoPage}>NEXT</button>
-                </center>
-                <center>
-                    <label className="skip" onClick={this.props.goToCompanyInfoPage} >Skip this step</label>
-                </center>
             </div>
         </div>
+	</div>
     );
 },
 });
--- src/vms/personalInfoPage.js
+++ src/vms/personalInfoPage.js
@@ -3,6 +3,18 @@ import React from 'react';
 const PersonalInfoSC = function(store) {
     //Components
     const PersonalInfo = require("components/PersonalInfo");
+    const EditPopUp = require("components/ProfileEdit");
+
+    function closeModal(caller) { 
+        store.dispatch({ 
+          type: 'NAV_SET_MODAL_BASE', 
+          payload: { 
+            caller: caller, 
+            modal: undefined 
+          } 
+        }); 
+    }
+
     return (
 		<PersonalInfo
             goToCompanyInfoPage={()=>{
@@ -23,6 +35,21 @@ const PersonalInfoSC = function(store) {
                 }});
                 //store.dispatch({type:'NAV_POP_BASE'});
             }}
+            editProfile={(modalId)=>{ 
+                store.dispatch({ 
+                    type: "NAV_SET_MODAL_BASE", 
+                    payload: { 
+                        modal: ( 
+                            <EditPopUp  
+                                id={modalId} 
+                                close={() => { 
+                                    closeModal('vms/userProfilePage') 
+                                }} 
+                            /> 
+                        ) 
+                    } 
+                }); 
+            }} 
         />
 	);
 
